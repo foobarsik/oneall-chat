4518e38692941887e0fdc6d9c8a156281ac0e74e
foobarsik <olgaraat@gmail.com>
2019-09-21 14:40:18 +0300

    add soft delete for bots

--- app/Entities/Bot.php
+++ app/Entities/Bot.php
@@ -3,9 +3,12 @@
 namespace App\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Bot extends Model
 {
+    use SoftDeletes;
+
     public const TYPE_TELEGRAM = 'telegram';
     public const TYPE_VIBER = 'viber';
     public const TYPES = [self::TYPE_TELEGRAM, self::TYPE_VIBER];
